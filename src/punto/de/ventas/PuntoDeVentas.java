4a1b48c8746dc39a2ac0bdb391d4c2435cac62cc
elmergustavo <elmergustavo79@gmail.com>
2022-05-05 23:20:39 -0600

    initial commit

---
+++ src/punto/de/ventas/PuntoDeVentas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto.de.ventas;
+
+import View.Login;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author AlexJPZ
+ */
+public class PuntoDeVentas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+           UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+          // UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        new Login().setVisible(true);
+        //maximizar la ventana
+//         Sistema main = new Sistema();
+//         main.setExtendedState(MAXIMIZED_BOTH);
+//         main.setVisible(true);
+    }
+
+}
